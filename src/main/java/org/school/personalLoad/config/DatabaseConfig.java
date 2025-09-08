b74b53ed015ad9094268437c25a065493c1542cc
Belyaev Dmitry <_email@gmail.com>
2025-09-08 23:13:48 +0300

    добавление анализа смены групп в3 работает fix сохранение преподавателей за старами группами

--- src/main/java/org/school/personalLoad/config/DatabaseConfig.java
+++ src/main/java/org/school/personalLoad/config/DatabaseConfig.java
@@ -2,7 +2,7 @@ package org.school.personalLoad.config;
 
 public class DatabaseConfig {
     // Сохранять ли историю изменений между запусками
-    public static final boolean KEEP_HISTORY = false;
+    public static final boolean KEEP_HISTORY = true;
 
     // Очищать ли историю при запуске (если KEEP_HISTORY = false)
     public static final boolean CLEAR_HISTORY_ON_START = false;
