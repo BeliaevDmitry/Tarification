d0a986255257bb8d838f1f4aec8f8aa5066bd7d1
Belyaev Dmitry <_email@gmail.com>
2025-10-05 23:42:23 +0300

    поправил константы адресов

--- src/main/java/org/school/personalLoad/config/AppConfig.java
+++ src/main/java/org/school/personalLoad/config/AppConfig.java
@@ -11,19 +11,19 @@ public class AppConfig {
             "https://docs.google.com/spreadsheets/d/1CgxahrURqJw79TtINoEsgfyZoVMO4NKuQxhk0NwDOHg/export?format=xlsx";
 
     // File names
-    public static final String TARIFFICATION_FILE_NAME = "Нагрузка 1 полугодие автоскачанный " +
+    public static final String TARIFFICATION_FILE_NAME = "Нагрузка 1 полугодие " +
             java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm"));
 
     public static final String PRACTICUM_FILE_NAME = "ЕГЭ 2026 автоскачанный";
 
     // Directories
-    public static final String DOWNLOAD_DIRECTORY = "C:\\Users\\dimah\\Desktop\\для расчёта тарификации\\выгрузки нагрузки\\";
-    public static final String OUTPUT_DIRECTORY =   "C:\\Users\\dimah\\Desktop\\для расчёта тарификации\\отчёт\\";
+    public static final String DOWNLOAD_DIRECTORY = "C:\\Users\\dimah\\Yandex.Disk\\1811\\для программ\\для расчёта тарификации\\выгрузки нагрузки\\";
+    public static final String OUTPUT_DIRECTORY =   "C:\\Users\\dimah\\Yandex.Disk\\1811\\для программ\\для расчёта тарификации\\отчёт\\";
 
     public static final String OFFLINE_FILES_DIRECTORY =
-            "C:\\Users\\dimah\\Desktop\\для расчёта тарификации\\журнал на 27.09.2025\\";
+            "C:\\Users\\dimah\\Yandex.Disk\\1811\\для программ\\для расчёта тарификации\\журнал на 27.09.2025\\";
     public static final String EXPELLED_FILE_PATH =
-            "C:\\Users\\dimah\\Desktop\\для расчёта тарификации\\Учащиеся. Только отчисленные на 03.10.2025.xlsx";
+            "C:\\Users\\dimah\\Yandex.Disk\\1811\\для программ\\для расчёта тарификации\\Учащиеся. Только отчисленные на 03.10.2025.xlsx";
 
     // Output file names
     public static final String TARIFFICATION_OUTPUT = "Тарификация" +
