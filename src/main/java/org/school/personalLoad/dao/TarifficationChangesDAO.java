507a27d856069eb81606187fe97d195a67ce09a8
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-02-19 11:49:16 +0300

    Revamp load distribution table columns and subgroup teacher rows

--- src/main/java/org/school/personalLoad/dao/TarifficationChangesDAO.java
+++ src/main/java/org/school/personalLoad/dao/TarifficationChangesDAO.java
@@ -1,5 +1,7 @@
 package org.school.personalLoad.dao;
 
+import org.springframework.stereotype.Repository;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.school.personalLoad.config.HibernateConfig;
@@ -7,6 +9,7 @@ import org.school.personalLoad.model.TarifficationChanges;
 
 import java.util.List;
 
+@Repository
 public class TarifficationChangesDAO {
 
     public void saveAll(List<TarifficationChanges> entities) {
---
+++ src/main/java/org/school/personalLoad/model/EducationLevel.java
@@ -0,0 +1,6 @@
+package org.school.personalLoad.model;
+
+public enum EducationLevel {
+    BASIC,
+    ADVANCED
+}
