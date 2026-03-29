cb7fd770b02ca38e3384977a3184f3fba6db1401
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-03-29 08:26:33 +0300

    Harden service memo generation/download before merge

---
+++ src/main/java/org/school/personalLoad/repository/ServiceMemoRepository.java
@@ -0,0 +1,17 @@
+package org.school.personalLoad.repository;
+
+import org.school.personalLoad.model.ServiceMemo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+public interface ServiceMemoRepository extends JpaRepository<ServiceMemo, Long> {
+    List<ServiceMemo> findAllByStatusOrderByCreatedAtDesc(ServiceMemo.Status status);
+
+    List<ServiceMemo> findAllByStatusInOrderByCreatedAtDesc(Collection<ServiceMemo.Status> statuses);
+
+    List<ServiceMemo> findAllByFioTeacherInAndStatusIn(Collection<String> fioTeachers, Collection<ServiceMemo.Status> statuses);
+}
