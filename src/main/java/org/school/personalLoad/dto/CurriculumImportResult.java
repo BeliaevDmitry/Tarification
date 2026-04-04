c95b18337ffb42fb32c9754712cdebff4f3ccfb6
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-04-04 21:31:27 +0300

    Добавил проверку сумм визуального импорта и подсветку расхождений

--- src/main/java/org/school/personalLoad/dto/CurriculumImportResult.java
+++ src/main/java/org/school/personalLoad/dto/CurriculumImportResult.java
@@ -3,6 +3,8 @@ package org.school.personalLoad.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class CurriculumImportResult {
@@ -12,4 +14,14 @@ public class CurriculumImportResult {
     private int classesCreated;
     private int orphanedLoads;
     private int subjectsImported;
+    private List<SumMismatch> sumMismatches;
+
+    @Data
+    @AllArgsConstructor
+    public static class SumMismatch {
+        private String classKey;
+        private String sumLabel;
+        private String expected;
+        private String actual;
+    }
 }
