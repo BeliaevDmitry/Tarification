168d75ffbaa042bbcd9242e7a006117b5e77821b
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-04-23 01:09:13 +0300

    Сделать year-привязку ОГЭ обязательной и добавить причину пустых нестыковок

---
+++ src/main/java/org/school/personalLoad/oge/repository/OgeWorkResultRepository.java
@@ -0,0 +1,13 @@
+package org.school.personalLoad.oge.repository;
+
+import org.school.personalLoad.oge.model.OgeWorkResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OgeWorkResultRepository extends JpaRepository<OgeWorkResult, Long> {
+    Optional<OgeWorkResult> findByAcademicYearAndFullNameAndSubjectName(String academicYear, String fullName, String subjectName);
+
+    List<OgeWorkResult> findAllByAcademicYearOrderByClassNameAscFullNameAscSubjectNameAsc(String academicYear);
+}
