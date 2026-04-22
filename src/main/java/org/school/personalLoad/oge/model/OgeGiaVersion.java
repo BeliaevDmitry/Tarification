b7c5f958d143d0f504f27695263b7cc33935b574
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-04-23 01:33:28 +0300

    Сократить подпись ссылки возврата на ВСОКО

---
+++ src/main/java/org/school/personalLoad/oge/model/OgeGiaVersion.java
@@ -0,0 +1,31 @@
+package org.school.personalLoad.oge.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "oge_gia_version")
+@Getter
+@Setter
+public class OgeGiaVersion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String sourceFileName;
+
+    @Column(length = 20)
+    private String academicYear;
+
+    @Column(nullable = false)
+    private LocalDateTime uploadedAt;
+
+    @OneToMany(mappedBy = "version", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OgeGiaParticipant> participants = new ArrayList<>();
+}
