f2729fea0c316ddbc047366ab5a701361aadb245
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-05-03 21:51:53 +0300

    Teachers: support editable initials/contacts/duties and hide created column

--- src/main/java/org/school/personalLoad/pa/service/PaService.java
+++ src/main/java/org/school/personalLoad/pa/service/PaService.java
@@ -13,8 +13,14 @@ import java.util.List;
 public interface PaService {
     List<PaDtos.ImportResult> importSpecifications(String academicYear, List<MultipartFile> files, String username);
     List<PaDtos.ImportLogRow> specificationImportLog(String academicYear, String username, boolean admin);
+    byte[] loadSpecificationImportLogFile(String academicYear, Long importLogId) throws IOException;
+    byte[] loadSpecificationImportFileByName(String academicYear, String fileName) throws IOException;
+    String specificationImportLogFileName(String academicYear, Long importLogId);
     List<PaDtos.SpecificationRow> specifications(String academicYear);
     List<PaDtos.SpecificationTaskRow> specificationTasks(Long specificationId);
+    void deleteSpecification(String academicYear, Long specificationId) throws IOException;
+    List<PaDtos.ClassLevelAssignmentRow> classLevelAssignments(String academicYear);
+    void saveClassLevelAssignments(String academicYear, List<PaDtos.ClassLevelAssignmentRow> rows);
     PaDtos.SummaryResponse summary(String academicYear);
     List<PaDtos.ReportVersionRow> reportVersions(String academicYear, String subjectName, PaScopeType scopeType, String scopeValue, PaLevel level, PaWorkType workType, LocalDate workDate);
     List<PaDtos.ReportWorkflowSummaryItem> reportWorkflowSummary(String academicYear, PaLevel level, PaWorkType workType, String subjectName);
@@ -26,5 +32,7 @@ public interface PaService {
     int deleteGeneratedReports(String academicYear, String subjectName, String scopeValue, boolean byParallel, PaLevel level, PaWorkType workType, LocalDate workDate);
     List<PaDtos.ReportFolderItem> reportFolderItems(String academicYear, PaWorkType workType);
     byte[] loadReportFile(Long reportVersionId) throws IOException;
+    String reportFileName(Long reportVersionId);
     byte[] loadSpecificationFile(String academicYear, Long specificationId) throws IOException;
+    String specificationFileName(String academicYear, Long specificationId);
 }
