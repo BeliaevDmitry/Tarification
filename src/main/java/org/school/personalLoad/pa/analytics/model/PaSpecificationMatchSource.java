05b352adaa84613a39f3d3fa254bf7984f1f4c0e
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-06-07 19:30:57 +0300

    Run PA analytics after accepted report upload

---
+++ src/main/java/org/school/personalLoad/pa/analytics/model/PaSpecificationMatchSource.java
@@ -0,0 +1,9 @@
+package org.school.personalLoad.pa.analytics.model;
+
+public enum PaSpecificationMatchSource {
+    CLASS_EXACT_DATE,
+    CLASS_NO_DATE,
+    PARALLEL_EXACT_DATE,
+    PARALLEL_NO_DATE,
+    NOT_FOUND
+}
